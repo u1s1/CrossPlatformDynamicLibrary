10f1dce808d2eeef364ce703ced274cff5e97963
zhangdongpeng <zhangdongpeng@zwcad.com>
2023-05-30 09:38:50 +0800

    feat:更改项目结构至更清晰。

--- Library/Interface/CrossPlatformLoad.h
+++ Library/Interface/CrossPlatformLoad.h
@@ -1,30 +1,8 @@
 #ifndef _CROSSPLATFORMLOAD_H_
 #define _CROSSPLATFORMLOAD_H_
 
+#include "ModuleLoad.h"
 #include "IMessage.h"
-
-#if defined(_WIN32)
-    #include <windows.h>
-    typedef HMODULE  MODULE_HANDLE;
-    MODULE_HANDLE hModule = LoadLibrary("CrossPlatformLib.dll");
-#else
-    #include <dlfcn.h>
-    typedef void *  MODULE_HANDLE;
-    MODULE_HANDLE hModule = dlopen("./libCrossPlatformLib.so", RTLD_NOW | RTLD_GLOBAL);
-#endif
-
-void *glo_GetModule(const char *pfname)
-{
-   if(hModule)
-   {
-#if defined(_WIN32)
-        return (void *)GetProcAddress(hModule, pfname);
-#else
-        return dlsym(hModule,pfname);
-#endif
-    }
-    return 0;
-};
 
 typedef void (*_VOID_)();
 typedef IMessage *(_IMessage_)(const char* m);
@@ -32,4 +10,4 @@ typedef IMessage *(_IMessage_)(const char* m);
 _VOID_ hello = (_VOID_)glo_GetModule("hello");
 _IMessage_* Message=(_IMessage_ *)glo_GetModule("NewMessage");
 
-#endif
+#endif          //_CROSSPLATFORMLOAD_H_
