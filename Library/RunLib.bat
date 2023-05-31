9b505e5ba284e9d60880184162fe5a1d668ab3b8
zhangdongpeng <zhangdongpeng@zwcad.com>
2023-05-31 15:52:07 +0800

    feat:win脚本可自动将生成库、第三方库及所需头文件放置在Out文件夹内。

--- Library/RunLib.bat
+++ Library/RunLib.bat
@@ -1,4 +1,4 @@
-if not exist build (  #判断文件夹build是否存在，不存在则就创建
+if not exist build (
     mkdir build
 ) else (
     del /S /Q /F build
@@ -9,4 +9,24 @@ if not exist build (  #判断文件夹build是否存在，不存在则就创建
 cd build
 cmake ..
 cmake --build .
+
+cd ..\..\
+if not exist Out (
+    mkdir Out
+) else (
+    del /S /Q /F Out
+    rd /S /Q Out
+    mkdir Out
+)
+
+xcopy Library\Interface\ Out\Interface\ /e
+copy ThirdLibWin\libxl\binx64\libxl.dll Out\
+copy Library\Bin64\Debug\CrossPlatformLib.dll Out\
+
+del /S /Q /F Library\Bin64
+rd /S /Q Library\Bin64
+
+del /S /Q /F Library\build
+rd /S /Q Library\build
+
 TIMEOUT /T -1
